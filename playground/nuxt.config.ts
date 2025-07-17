b8697b6429aa9a2c769d77ff5494320f67361e7d
atmoner <contact.atmoner@gmail.com>
2025-07-17 23:42:32 +0200

    fix: standardize formatting of baseURL assignment in nuxt.config.ts

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,9 @@
 export default defineNuxtConfig({
-  app: {
-    baseURL: process.env.NODE_ENV === "production" ? "https://atmoner.github.io/nuxt-auth-jwt/" : "https://atmoner.github.io/nuxt-auth-jwt/",
-  },
   modules: ['../src/module', '@nuxt/ui'],
   devtools: { enabled: true },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? 'https://atmoner.github.io/nuxt-auth-jwt/' : 'https://atmoner.github.io/nuxt-auth-jwt/',
+  },
   css: ['~/assets/css/main.css'],
   authJwt: {
     jwtSecret: 'my-super-secret-key-for-development',
